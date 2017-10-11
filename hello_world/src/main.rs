672d294014efc4d036e423cf3116a4ed4bbc8c0b
jiang1991 <jiang12196@126.com>
2017-10-11 23:34:48 +0800

    add gitignore

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    let numbers = vec![34, 50, 100, 65];
+    // let result = largest(&numbers);
+    println!("The largest number is {}", numbers[0]);
+    
+    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    // let result = largest(&numbers);
+    println!("The largest number is {}", numbers[1]);
 }
