66961db1b4871ce4019fb6c910a3e1f01d23dddd
jiang1991 <jiang12196@126.com>
2017-10-21 11:03:29 +0800

    fix ' - fix mod file.rs; -> mod file; - fix clap::{APP} -> clap::{App} - fix matches function

--- passwd/src/super_password_cli.rs
+++ passwd/src/super_password_cli.rs
@@ -1,23 +1,23 @@
 extern crate oath;
 extern crate clap;
 
-use clap::{Arg, APP};
+use clap::{Arg, App};
 
-mod generate_password.rs;
+mod generate_password;
 
 fn main() {
     let matchs = App::new("tx super password generator")
-        .arg(Arg::with_name("orgid"))
+        .arg(Arg::with_name("orgid")
             .long("orgid")
             .value_name("ID")
             .help("input organization ID")
             .takes_value(true)
-            .required(true)
+            .required(true))
         .get_matches();
 
     let org_id = matchs.value_of("orgid").unwrap();
 
-    let super_password = generate_password::generate_password(&org_id);
+    let super_password = generate_password::generate_super_password(&org_id);
     
     println!("password: {}", super_password);
 }
